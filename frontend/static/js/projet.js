01c2e02a70e07ca19ca63cc91695f4fbdcdc62dc
nathan mimoun <nathan.mimoun@gmail.com>
2019-12-24 18:30:08 +0100

    fixed map issues and removed innerHTML

--- frontend/static/js/projet.js
+++ frontend/static/js/projet.js
@@ -19,12 +19,94 @@ function ajaxPost(url, data, callback) {
 }
 
 function display(obj) {
+    buildAnswerBox(x, obj.robot, obj.address, obj);
+}
+
+function buildQuestionBox(display, avatar_url, question) {
+    // Création du container
+    let containerElt = document.createElement("div");
+    containerElt.classList.add("container", "darker");
+    display.appendChild(containerElt);
+
+    // Création de l'élement image
+    let avatarElt = document.createElement("img");
+    avatarElt.src = avatar_url;
+    avatarElt.alt = "Avatar";
+    avatarElt.classList.add("right", "avatar");
+    containerElt.appendChild(avatarElt);
+
+    // Création du paragraphe
+    let paragraphElt = document.createElement("p");
+    paragraphElt.textContent = question;
+    containerElt.appendChild(paragraphElt);
+}
+
+function buildAnswerBox(display, avatar_url, address, obj) {
     var resultat = obj.wiki;
-    x.innerHTML += "<div class=\"container\"><img src=\"" + obj.robot + "\" alt=\"Avatar\" style=\"width:100%;\"><p> Voici l'adresse que tu recherche: " + obj.address + "</p></div>";
+    // Création du container
+    let containerElt = document.createElement("div");
+    containerElt.classList.add("container");
+    display.appendChild(containerElt);
+
+    let boite = document.createElement("div");
+    boite.classList.add("message");
+    containerElt.appendChild(boite);
+
+    // Création de l'élement image
+    let avatarElt = document.createElement("img");
+    avatarElt.src = avatar_url;
+    avatarElt.alt = "Avatar";
+    avatarElt.classList.add("avatar");
+    boite.appendChild(avatarElt);
+
+    // Création du paragraphe
+    let paragraphElt = document.createElement("p");
+    paragraphElt.textContent = " Voici l'adresse que tu recherche: " + address;
+    boite.appendChild(paragraphElt);
+
+    let mapElt = document.createElement("div");
+    mapElt.setAttribute("id", "map");
+    containerElt.appendChild(mapElt);
+    createMap(obj, mapElt);
+
+    let parapar = document.createElement("div");
+    parapar.classList.add("container");
+    display.appendChild(parapar);
+
+    let avatar = document.createElement("img");
+    avatar.src = avatar_url;
+    avatar.alt = "Avatar";
+    avatar.classList.add("avatar");
+    parapar.appendChild(avatar);
+
+    let para = document.createElement("div");
+    para.classList.add("response");
+    parapar.appendChild(para);
+
+    let yeo = document.createElement("p");
+    yeo.textContent = resultat[0]["abstract"];
+    para.appendChild(yeo);
+
+    let imi = document.createElement("img");
     var no_thumb = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Gnome-image-missing.svg/200px-Gnome-image-missing.svg.png";
     var thumbnail = resultat[0].thumbnail || no_thumb;
-    // affichage de la reponse dans la page de maniere visuelle
-    x.innerHTML += "<div class=\"container\"> <img src=\"" + obj.robot + "\" alt=\"Avatar\" style=\"width:100%;\"><div class=\"response\"><p>" + resultat[0]["abstract"] + "</p><img src=\"" + resultat[0]["thumbnail"] + "\"id=\"wikip\" alt=\"wikipedia\"></div>";
+    imi.src = thumbnail;
+    imi.alt = "wikipedia";
+    imi.setAttribute("id", "wikip");
+    para.appendChild(imi);
+}
+
+function createMap(obj, mapElt) {
+    var localisation = obj.here;
+    var latitude = localisation[0];
+    var longitude = localisation[1];
+    const platform = new H.service.Platform({ "app_id": "W82jOVCtSiQ4dZHBaU8e", "app_code": "2J6YA4nvRMB_IHJlwo7uXQ" });
+    const map = new H.Map(mapElt, platform.createDefaultLayers().normal.map, { zoom: 17, center: { lat: latitude, lng: longitude } });
+    const mapEvent = new H.mapevents.MapEvents(map);
+    const mapBehavior = new H.mapevents.Behavior(mapEvent);
+    const marker = new H.map.Marker({ lat: latitude, lng: longitude });
+    map.addObject(marker);
+
 }
 
 // formulaire texte et envoie a flask en POST
@@ -42,18 +124,10 @@ form.addEventListener("submit", function (e) {
         var obj = JSON.parse(response);
         var question = data.get('Text1');
         // affichage de la question de maniere visuelle
-        x.innerHTML += "<div class=\"container darker\"><img src=\"" + obj.avatar + "\" alt=\"Avatar\" class=\"right\" style=\"width:100%;\"><p>" + question + "</p></div>";
+        buildQuestionBox(x, obj.avatar, question)
+
         var status = obj.status;
         if (status == 'true') {
-            var localisation = obj.here;
-            var latitude = localisation[0];
-            var longitude = localisation[1];
-            const platform = new H.service.Platform({ "app_id": "W82jOVCtSiQ4dZHBaU8e", "app_code": "2J6YA4nvRMB_IHJlwo7uXQ" });
-            const map = new H.Map(document.getElementById("map"), platform.createDefaultLayers().normal.map, { zoom: 17, center: { lat: latitude, lng: longitude } });
-            const mapEvent = new H.mapevents.MapEvents(map);
-            const mapBehavior = new H.mapevents.Behavior(mapEvent);
-            const marker = new H.map.Marker({ lat: latitude, lng: longitude });
-            map.addObject(marker);
             display(obj);
         } else {
             x.innerHTML += "<div class=\"container\"> <img src=\"" + obj.robot + "\" alt=\"Avatar\" style=\"width:100%;\"><div class=\"response\"><p> je suis désolé mais je n'ai pas compris la question.. </p></div>";
