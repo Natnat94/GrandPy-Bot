fbfc325b739f883dcb3444f5883774d1a766e5e4
nathan mimoun <nathan.mimoun@gmail.com>
2020-01-07 10:45:54 +0100

    bug fix

--- test_helloworld.py
+++ test_helloworld.py
@@ -1,39 +1,86 @@
-import parser_man as parser
+from parser_man import Localisation as localisation
 import requests
 
 
 def test_parser():
-    pars = parser.parser("ou se situe le pantheon")
-    assert pars == "le pantheon"
+    pars = localisation()
+    rien = pars.parser("ou se situe le pantheon")
+    assert rien == "le pantheon"
 
 def test_wiki_api(monkeypatch):
-    FAKE_API_RESULT= {'batchcomplete': '','query': {'pages': {'18618509': {'coordinates': [{'globe': 'earth',
-                                                   'lat': 37.7891838,
-                                                   'lon': -122.4033522,
-                                                   'primary': ''}],
-                                  'ns': 0,
-                                  'pageid': 18618509,
-                                  'title': 'Wikimedia Foundation'}}}}
+    FAKE_API_RESULT={"batchcomplete": "",
+                    "query": {
+                        "pages": {
+                            "1359783": {
+                                "pageid": 1359783,
+                                "ns": 0,
+                                "title": "Tour Eiffel",
+                                "index": -1,
+                                "coordinates": [
+                                    {
+                                        "lat": 48.858296,
+                                        "lon": 2.294479,
+                                        "primary": "",
+                                        "globe": "earth"
+                                    }
+                                ],
+                                "thumbnail": {
+                                    "source": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/78px-Tour_Eiffel_Wikimedia_Commons.jpg",
+                                    "width": 78,
+                                    "height": 144
+                                },
+                                "pageimage": "Tour_Eiffel_Wikimedia_Commons.jpg",
+                                "extract": "La tour Eiffel  est une tour de fer puddl\u00e9 de 324 m\u00e8tres de hauteur ",
+                                "contentmodel": "wikitext",
+                                "pagelanguage": "fr",
+                                "pagelanguagehtmlcode": "fr",
+                                "pagelanguagedir": "ltr",
+                                "touched": "2020-01-05T20:19:17Z",
+                                "lastrevid": 166075414,
+                                "length": 128888,
+                                "fullurl": "https://fr.wikipedia.org/wiki/Tour_Eiffel",
+                                "editurl": "https://fr.wikipedia.org/w/index.php?title=Tour_Eiffel&action=edit",
+                                "canonicalurl": "https://fr.wikipedia.org/wiki/Tour_Eiffel"
+                            }}}}
 
     class MockRequestsGet:
-
-        @staticmethod
+        def __init__(self, url, params):
+            pass
         def json(self):
             return FAKE_API_RESULT
 
-    monkeypatch.setattr('requests.get', MockRequestsGet)
-    result = parser.wiki_api("Wikimedia Foundation")
-    assert result == FAKE_API_RESULT
+    monkeypatch.setattr('requests.Session.get', MockRequestsGet)
+    pars = localisation()
+    result = pars.wiki_api([48.85824, 2.2945])
+    assert result[0]["title"] == "Tour Eiffel"
+    assert result[0]["abstract"] == "La tour Eiffel  est une tour de fer puddl\u00e9 de 324 m\u00e8tres de hauteur "
+    assert result[0]["articleUrl"] == "https://fr.wikipedia.org/wiki/Tour_Eiffel"
 
 def test_map_api(monkeypatch):
-    FAKE_API_RESULT= [48.85824, 2.2945]
-
+    FAKE_API_RESULT= {"results": {
+        "items": [
+            {
+                "position": [
+                    48.85824,
+                    2.2945
+                ],
+                "distance": 5808,
+                "title": "Eiffel Tower",
+                "averageRating": 0.0,
+                "icon": "https://download.vcdn.cit.data.here.com/p/d/places2_stg/icons/categories/38.icon",
+                "vicinity": "5 Avenue Anatole France<br/>75007 Paris",
+            }
+        ]}
+}
     class MockRequestsGet:
 
-        @staticmethod
+        def __init__(self, url, params):
+            pass
         def json(self):
             return FAKE_API_RESULT
 
-    monkeypatch.setattr('requests.get', MockRequestsGet)
-    result = parser.map_api("Tour Eiffel", "48.8482,2.3724;r=245799")
-    assert result['results']['items'][0]['position']  == FAKE_API_RESULT
+    monkeypatch.setattr('requests.Session.get', MockRequestsGet)
+    pars = localisation ()
+    geoloc, address = pars.map_api("Tour Eiffel", "48.8482,2.3724;r=245799")
+    assert geoloc  == ['48.85824', '2.2945']
+    assert address  == "5 Avenue Anatole France 75007 Paris"
